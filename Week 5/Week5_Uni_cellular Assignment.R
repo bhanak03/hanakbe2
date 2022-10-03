fb32d65fb7329b1392f1d6b69463784cf2ceaedd
Bethany Hanak <hanak@susqu.edu>
2022-10-03 11:03:26 -0400

    Update Week5_Uni_cellular Assignment.R

--- Week 5/Week5_Uni_cellular Assignment.R
+++ Week 5/Week5_Uni_cellular Assignment.R
@@ -7,6 +7,11 @@
 setwd("C:/GitHub/hanakbe2/Week 5")
 data <- read.csv("Figure4Data.csv", header=TRUE)
 
+
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+
 data.new <- data.frame(data[1:4])
 data.new$freq <- data.new$Num.Cells.Progeny
 data.new <- na.omit(data.new)
@@ -67,15 +72,16 @@ myplot <- ggplot(data = data.weight,
   annotate('text', x=8, y=-0.5, label="115") +
   annotate('text', x=9, y=-0.5, label="103")
 
-ggsave(filename=paste("C:/GitHub/hanakbe2/Week 5",
-                      paste("Figure4", ".png", sep=""), sep=""), plot=myplot,
-       width = 10, height = 4, bg = 'transparent')
+
 
 
 # Do all of the strains in the plot have the same distributions (yes/no)? (2 pt)
 
+#no
+
 # Based on these observations of your strain distributions, why did the authors use a Kruskal-Wallis test rather than ANOVA to compare the strains? (2 pts)
 
+#a Kruskal-Wallis test doesn't assume a normal distribution like an anova does, making it a better fit for non-normal variables.
 
 # Use the fitdist() and gofstat() functions to compare the poisson, negative binomial, and logistic distributions for:
   # (1) - The number of cells of progeny (data$Num.Cells.Progeny)
@@ -84,19 +90,45 @@ ggsave(filename=paste("C:/GitHub/hanakbe2/Week 5",
     #HINT- "Num.Cells.Progeny" has defined breaks. To display results, use the formula with the "chisqbreaks" argument as follows:
       #gofstat(list(fit.1, fit.2, fit.3, etc), chisqbreaks=c(1,2,4,8,16,32,64))
 
+?fitdist
+
+one.col <- data$Num.Cells.Progeny
+fit.pois <- fitdist(c(na.exclude(one.col)), distr = "pois")
+fit.logis <- fitdist(c(na.exclude(one.col)), distr = "logis")
+fit.nbinom <- fitdist(c(na.exclude(one.col)), distr = "nbinom")
+
+
+gofstat(list(fit.pois, fit.nbinom, fit.logis),chisqbreaks=c(1,2,4,8,16,32,64))
+
+
+sec.col <- data$RepTime.sec
+fit.pois2 <- fitdist(c(na.exclude(sec.col)), distr = "pois")
+fit.logis2 <- fitdist(c(na.exclude(sec.col)), distr = "logis")
+fit.nbinom2 <- fitdist(c(na.exclude(sec.col)), distr = "nbinom")
+
+
+gofstat(list(fit.pois2, fit.nbinom2, fit.logis2))
 
 # Based on the AIC scores, which distribution is the best fit for: (4 pts)
   # (1) - The number of cells of progeny (data$Num.Cells.Progeny)?
+
+#negative binomial because it has the lowest AIC score.
+
   # (2) - The replication time (data$RepTime.sec)?
 
+#negative binomial, also has the lowest AIC score.
 
 # Plot a generic histogram for the replication time (data$RepTime.sec) (2 pt)
+
+hist(data$RepTime.sec)
+
 
 # Based on the patterns of this histograms and Figure 4:
   #Give one hypothesis for an evolutionary process represented by the two tallest bars in your histogram. (6 pts)
   # Don't cheat by looking at the paper! 
     # This hypothesis does not need to be correct - it only needs to be ecologically rational based these two figures.
-
+#The tallest bars most likely represent functions that are essential to life, things relating to fitness like gamete production (or fission rate in the case of microbes),
+#or growth/maturation of cells. 
 
 
 
