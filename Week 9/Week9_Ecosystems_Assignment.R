5125d7a6d171bc0d45f9d608a6de4833428c7200
bhanak03 <hanak@susqu.edu>
2022-10-27 09:53:09 -0400

    Update Week9_Ecosystems_Assignment.R

--- Week 9/Week9_Ecosystems_Assignment.R
+++ Week 9/Week9_Ecosystems_Assignment.R
@@ -14,6 +14,30 @@ abiotic <- as.data.frame(abiotic.tibble)
 invert.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Invertebrate_community")
 invert <- as.data.frame(invert.tibble)
 head(invert)
+
+abiotic.names <- paste(abiotic$Parcel, abiotic$Land_Use)
+abiotic$names <- abiotic.names
+
+invert.names <- paste(invert$Parcel, invert$Landuse)
+invert$names <- invert.names
+
+abiotic.means <- aggregate(x = abiotic, by = list(abiotic$names), FUN = "mean")
+invert.means <- aggregate(x = invert, by = list(invert$names), FUN = "mean")
+
+head(abiotic.means)
+head(invert.means)
+
+abiotic.means1 <- abiotic.means[,c(-2,-3,-5,-6,-16)]
+invert.means1 <- invert.means[,-2:-3]
+
+invert.means2 <- sapply(invert.means1, as.numeric)
+
+
+library(vegan)
+colnames(abiotic.means1)
+ord <- rda(invert.means2 ~ pH + totalN + Perc_ash + Kalium + Magnesium + Ca + Al + TotalP + OlsenP, abiotic.means1)
+ord
+
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
 
